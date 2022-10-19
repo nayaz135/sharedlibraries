89b8a38e69b79d7fa34bf07b15ba4439cd4f6cd6
nayaz135 <114727218+nayaz135@users.noreply.github.com>
2022-10-19 21:33:35 +0530

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,31 @@
+def sum(a,b)
+
+{
+
+The sum of ${a} and ${b} is ${a+b}
+
+}
+
+def sum(a,b)
+
+{
+
+The sub of ${a} and ${b} is ${a-b}
+
+}
+
+def mul(a,b)
+
+{
+
+The mul of ${a} and ${b} is ${a*b}
+
+}
+
+def div(a,b)
+
+{
+
+The div of ${a} and ${b} is ${a/b}
+
+}
